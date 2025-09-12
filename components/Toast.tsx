afd9ef932575aee0c8372b92205c52b6f4520dbe
leoxavier3914-arch <leoxavier3914@gmail.com>
2025-09-12 07:17:37 -0300

    Enable strict TypeScript mode

--- components/Toast.tsx
+++ components/Toast.tsx
@@ -1,9 +1,7 @@
 'use client';
 
-import { Toaster, toast } from 'react-hot-toast';
+import { Toaster } from 'react-hot-toast';
 
 export default function Toast() {
   return <Toaster position="top-right" />;
 }
-
-export { toast };
